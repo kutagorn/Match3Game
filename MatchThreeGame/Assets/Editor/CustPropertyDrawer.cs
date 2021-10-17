aafd31bc759f5558d51e34bb501c47b4f901bfdb
Kutay Can Kaynak <ktykynk814@gmail.com>
2021-10-17 14:06:57 +0300

    first iteration

---
+++ MatchThreeGame/Assets/Editor/CustPropertyDrawer.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+
+[CustomPropertyDrawer(typeof(ArrayLayout))]
+public class CustPropertyDrawer : PropertyDrawer {
+
+	public override void OnGUI(Rect position,SerializedProperty property,GUIContent label){
+		EditorGUI.PrefixLabel(position,label);
+		Rect newposition = position;
+		newposition.y += 18f;
+		SerializedProperty data = property.FindPropertyRelative("rows");
+        if (data.arraySize != 8)
+            data.arraySize = 8;
+		//data.rows[0][]
+		for(int j=0;j<8;j++){
+			SerializedProperty row = data.GetArrayElementAtIndex(j).FindPropertyRelative("row");
+			newposition.height = 18f;
+			if(row.arraySize != 8)
+				row.arraySize = 8;
+			newposition.width = position.width/9;
+			for(int i=0;i<8;i++){
+				EditorGUI.PropertyField(newposition,row.GetArrayElementAtIndex(i),GUIContent.none);
+				newposition.x += newposition.width;
+			}
+
+			newposition.x = position.x;
+			newposition.y += 18f;
+		}
+	}
+
+	public override float GetPropertyHeight(SerializedProperty property,GUIContent label){
+		return 18f * 15;
+	}
+}
---
+++ MatchThreeGame/Assets/Scripts/Match3.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Match3 : MonoBehaviour
+{
+    public ArrayLayout boardLayout;
+    int width = 8;//Board un eni
+    int height = 8;//Board un boyu
+    Node[,] board;
+
+    System.Random random;
+    void Start()
+    {
+        StartGame();
+    }
+    void StartGame()
+    {
+        string seed = getRandomSeed();
+        random = new System.Random(seed.GetHashCode());
+        InitializeBoard();
+    }
+    void InitializeBoard() 
+    {
+        //board un yaratılısı
+        board = new Node[width, height];
+        
+        for(int y = 0; y < height; y++)
+            {
+                for (int x = 0; x < width; x++)
+                {
+                    board[x, y] = new Node(-1, new Point(x,y));
+                }
+            }
+    }
+    void Update()
+    {
+    
+    }
+    string getRandomSeed()
+    {
+        string seed = "";
+        string acceptableChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890!@#$%^&*()";
+        for(int i = 0; i < 20; i++)
+            {
+                seed += acceptableChars[Random.Range(0, acceptableChars.Length)];
+            }
+        return seed;
+    }
+}
+[System.Serializable]
+public class Node 
+{
+    public int value; // 0 - Bos,1 - Mavi, 2 - Yesil, 3 - Kırmızı, 4 - Sarı, -1 Cukur
+    public Point index;
+
+    public Node(int v, Point i)
+    {
+        value = v;
+        index = i;
+    }
+}
